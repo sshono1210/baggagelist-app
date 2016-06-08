889369cb868636cfcfca5ea691eb5003387228ab
mikakane <t.goto@chatbox-inc.com>
2016-06-08 19:48:11 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,13 +24,13 @@ var tasks = {};
 })
 
 gulp.task("sass",()=> tasks.sass.bourbon());
-gulp.task("watch:sass",()=> gulp.watch(tasks.sass.target,"sass") );
+gulp.task("watch:sass",()=> gulp.watch(tasks.sass.target,["sass"]) );
 
 gulp.task("jade",()=> tasks.jade.build());
-gulp.task("watch:jade",()=> gulp.watch(tasks.jade.target,"jade") );
+gulp.task("watch:jade",()=> gulp.watch(tasks.jade.target,["jade"]) );
 
 gulp.task("webpack",() => tasks.webpack.build());
-gulp.task("watch:webpack",() => gulp.watch(tasks.webpack.target,"webpack") );
+gulp.task("watch:webpack",() => gulp.watch(tasks.webpack.target,["webpack"]) );
 
 //gulp.task("ws:preview",() => tasks.wintersmith.preview("./wintersmith.json"));
 //gulp.task("ws:build",  () => tasks.wintersmith.build("./wintersmith.json"));
