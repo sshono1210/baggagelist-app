772644327b03769f68bc297f0fa59b9c7e83e9e0
sshono <s.shosho1210@gmail.com>
2016-10-19 10:44:29 +0900

    全画面SPAに

--- public/common.js
+++ public/common.js
@@ -14,22 +14,44 @@ var New = Vue.extend({
         });
     }
 });
-
+var Edit = Vue.extend({
+    template: '#l-edit',
+    mounted:function(){ //コンポーネント読み込むときにアコーディオンが動くようにする
+        console.log($('.collapsible'))
+        $('.collapsible').collapsible({
+            accordion : false
+        });
+    }
+});
 // ルーターの定義
 const routes = [
     { path: '/', component: Index },
     { path: '/detail', component: Detail },
-    { path: '/new', component: New }
+    { path: '/new', component: New },
+    { path: '/edit', component: Edit }
 ];
-
 // ルーターの登録
 const router = new VueRouter({
     routes:routes // routes: routes の短縮表記
 });
+
+
+var object = {
+    data1: "apple",
+    data2: "lemon",
+    data3: "melon"
+};
+// データの保存
+localStorage.setItem('test', JSON.stringify(object));
+// データの取得
+console.log(JSON.parse(localStorage.getItem('test')));
+
 
 $(function(){
     var app = new Vue({
         el: "#app",
         router: router
     });
+
+
 });
